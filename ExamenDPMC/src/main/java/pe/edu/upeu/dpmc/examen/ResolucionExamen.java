90c6819ef377912d235b189d5e770bcb41c265e8
fienker <darwin.mamani@upeu.edu.pe>
2021-06-23 01:59:53 -0500

    examen

--- ExamenDPMC/src/main/java/pe/edu/upeu/dpmc/examen/ResolucionExamen.java
+++ ExamenDPMC/src/main/java/pe/edu/upeu/dpmc/examen/ResolucionExamen.java
@@ -6,6 +6,51 @@
   */
  public class ResolucionExamen {
 
+    public static void Impuesto() {
+        Scanner in = new Scanner(System.in);
+        int i, n;
+        double categoria_1, categoria_2, categoria_3, clave, costo;
+        double impuesto, impuesto_a_pagar;
+        categoria_1 = 0;
+        categoria_2 = 0;
+        categoria_3 = 0;
+        impuesto_a_pagar = 0;
+        System.out.print("Ingresa el valor del auto: ");
+        n = in.nextInt();
+        in.nextLine();
+        for (i=1; i<=n; i++) {
+            System.out.print("Ingresa la categoria: ");
+            clave = in.nextDouble();
+            in.nextLine();
+            costo = in.nextDouble();
+            in.nextLine();
+            impuesto=0;
+            if(clave==1)
+            {
+                impuesto=costo*0.1;
+                categoria_1=categoria_1+impuesto;
+            }
+            if(clave==2)
+            {
+                impuesto=costo*0.07;
+                categoria_2=categoria_2+impuesto;
+            }
+            if(clave==3)
+            {
+                impuesto=costo*0.05;
+                categoria_3=categoria_3+impuesto;
+            }
+            impuesto_a_pagar=impuesto_a_pagar+impuesto;
+            System.out.println("Valor de impuesto: " + impuesto);
+            System.out.println();
+        }
+        System.out.println("Valor de categoria 1: " + categoria_1);
+        System.out.println("Valor de categoria 2: " + categoria_2);
+        System.out.println("Valor de categoria 3: " + categoria_3);
+        System.out.println("Valor de impuesto a pagar: " + impuesto_a_pagar);
+    }
+
+
      public static void TablaMultiplicar() {
         int Multiplicando ;
             System.out.println("¿Que tabla desea Realizar?");
@@ -46,8 +91,9 @@
 
     public static void main(String[] args){
         //System.out.println("hola mundo");
+        Impuesto();
         //TablaMultiplicar();
-        NUmerosPerfectos();
+        //NUmerosPerfectos();
         
     }
  
