7fbfa1e2cfa2fdc8c63b1bfbac5d4e14b7109755
fienker <darwin.mamani@upeu.edu.pe>
2021-06-23 00:40:25 -0500

    examen

--- ExamenDPMC/src/main/java/pe/edu/upeu/dpmc/examen/ResolucionExamen.java
+++ ExamenDPMC/src/main/java/pe/edu/upeu/dpmc/examen/ResolucionExamen.java
@@ -6,40 +6,48 @@
   */
  public class ResolucionExamen {
 
-     public static void numerosPerfectos() {
-        int i, j, suma;
-        System.out.println("Los 3 primeros numeros perfectos son: ");
-        for(i=1;i <= 1000;i++){   
-            suma = 0;
-            for(j = 1;j < i;j++){                        
-                 if(i % j==0){
-                    suma = suma + j; 
-                 }
-            }
-          if(i == suma){                         
-             System.out.println(i);
-          }
-        }
-     }
-
      public static void TablaMultiplicar() {
         int Multiplicando ;
             System.out.println("¿Que tabla desea Realizar?");
             Scanner valor1 = new Scanner(System.in);
             Multiplicando = valor1.nextInt();
-
             int multiplicador = 1;
-            do {   System.out.println (Multiplicando + " X " +            multiplicador + " = " + Multiplicando * multiplicador );
-
+            do {   System.out.println (Multiplicando + " X " + multiplicador + " = " + Multiplicando * multiplicador );
                    multiplicador += 1;
+        } while (multiplicador<=20);     
+    }
 
-        } while (multiplicador<=10);     
+    public static void NUmerosPerfectos(){
+
+        Scanner lector = new Scanner(System.in); //Utilidad de interacció
+
+        System.out.println("Empezando listado:"); //Utilidad para escribir en pantalla
+        int m = 1;
+        do{
+        m = m + 1;
+        int n = 0;
+        int c = 0;
+        int d = 0;
+        int f = 0;
+        do{
+        f = m - 1;
+        n = n + 1;
+        c = m % n;
+        if (c == 0){
+        d = d + n;
+        }
+        }while (n != f);
+        if (d == m){
+        System.out.println("el numero perfecto es: " + m);
+        }
+        }while(m <= 1000 );
     }
      
 
     public static void main(String[] args){
         //System.out.println("hola mundo");
-        TablaMultiplicar();
+        //TablaMultiplicar();
+        NUmerosPerfectos();
         
     }
  
